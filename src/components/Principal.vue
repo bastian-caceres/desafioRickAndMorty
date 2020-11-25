be30c46dfbade9afa67e6cc23d2342d49ba44572
bastian caceres <bast.caceresr@gmail.com>
2020-11-25 18:29:02 -0300

    terminado

---
+++ src/components/Principal.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <section class="container text-justify">
+      <!-- tercer paso -->
+      <Table :dataUsers="personajes"/>
+    </section>
+  </main>
+</template>
+
+<script>
+/* primer paso */
+import Table from './mainHijos/Table';
+
+export default {
+  name: 'Principal',
+  data() {
+      return {
+          personajes: [
+            {nombre: "Rick Sanchez", src:"https://rickandmortyapi.com/api/character/avatar/1.jpeg"},
+            {nombre: "Morty Smith", src:"https://rickandmortyapi.com/api/character/avatar/2.jpeg"},
+            {nombre: "Summer Smith", src:"https://rickandmortyapi.com/api/character/avatar/3.jpeg"},
+            {nombre: "Beth Smith", src:"https://rickandmortyapi.com/api/character/avatar/4.jpeg"},
+            {nombre: "Jerry Smith", src:"https://rickandmortyapi.com/api/character/avatar/5.jpeg"},
+            {nombre: "Abadango Cluster Princess", src:"https://rickandmortyapi.com/api/character/avatar/6.jpeg"},
+          ]
+      }
+  },
+  // segundo paso
+  components:{
+    Table
+  }
+}
+</script>
+
+<style scoped>
+   main{
+    background-image: url('https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* height: 100%; */
+  }
+</style>
---
+++ src/components/mainHijos/Table.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="container">
+        
+        <div v-for="(item, index) in dataUsers" :key="index">
+            <div class="mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img :src="item.src" :alt="item.nombre" class="card-img rounded-circle">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body text-white">
+                            <h5 class="card-title">{{item.nombre}}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Table',
+    props: {
+        dataUsers: {
+            type: Array,
+            required: true,
+            default(){
+                return [
+                    {nombre: 'No tiene', src: 'no tiene'}
+                ]
+            } 
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
